2e8c62a18dae7842cf99a3ca46c878dc8374a635
Kelsea <kelsea.best@gmail.com>
2024-02-29 16:35:57 -0500

    data push

--- scripts/just_censusdata.R
+++ scripts/just_censusdata.R
@@ -7,8 +7,8 @@ library(tigris)
 
 ######## next step is to pull in block group census data from ACS ########
 
-insp_w_census <- read_csv("data/locations_inspectionscores_forMeri_Feb.csv") 
-
+insp_w_census <- read_csv("data/locations_inspectionscores_forMeri_Feb.csv") %>%
+  mutate(tract = substr(as.character(block_group), 1, 11))
 locations_only <- insp_w_census %>%
   select(c(1, 16, 17)) %>%
   mutate(tract = substr(as.character(block_group), 1, 11))%>%
@@ -21,7 +21,7 @@ census_api_key("ff5d487d0a2a22c658bf319ba136c27db32aa0be", install = TRUE,
 blockgroups <- unique(na.omit(insp_w_census$block_group))
 counties <- unique(na.omit(insp_w_census$county))
 states <- unique(na.omit(insp_w_census$STATE_CODE))
-insp_years <- c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020)
+insp_years <- c(2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020)
 
 
 
@@ -90,6 +90,7 @@ for (i in insp_years){
   
 }
 
+#write_csv(hold_data_c, "data/raw_county_data.csv")
 
 all_county_demographics <- locations_only %>%
   left_join(hold_data_c, by = c("county" = "GEOID"))
@@ -107,18 +108,18 @@ hold_data_t <- data.frame()
 for (i in insp_years){
   
   other_census_blockgroup <- get_acs(survey = "acs5", geography = "tract", 
-                                     variables = c(age_c = "B01002_001",
-                                                   income_c =  "B19013_001",
-                                                   prop_value_c = "B25077_001",
-                                                   public_assistance_c = "B19057_001",  
-                                                   poverty_c = "B17010_001", 
-                                                   vehicles_c = "B25044_001",
-                                                   renter_c = "B25003_003",
-                                                   owner_c = "B25003_002",
-                                                   total_white_c = "B03002_003",
-                                                   total_black_c = "B03002_004",
-                                                   total_hispanic_c = "B03002_012",
-                                                   total_pop_c = "B01003_001"),
+                                     variables = c(age = "B01002_001",
+                                                   income =  "B19013_001",
+                                                   prop_value = "B25077_001",
+                                                   public_assistance = "B19057_001",  
+                                                   poverty = "B17010_001", 
+                                                   vehicles = "B25044_001",
+                                                   renter = "B25003_003",
+                                                   owner = "B25003_002",
+                                                   total_white = "B03002_003",
+                                                   total_black = "B03002_004",
+                                                   total_hispanic = "B03002_012",
+                                                   total_pop = "B01003_001"),
                                      state = c(unique(states)), 
                                      year = i)
   
@@ -132,6 +133,7 @@ for (i in insp_years){
   
 }
 
+#write_csv(hold_data_t, "data/raw_tract_data.csv")
 
 all_tract_demographics <- locations_only %>%
   left_join(hold_data_t, by = c("tract" = "GEOID"))
