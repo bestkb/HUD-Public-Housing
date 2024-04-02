7da3d364e25a88c4419ba6421d52a171f7e1947a
Yusen Peng <yusenrocks@gmail.com>
2024-04-02 17:58:07 -0400

    normalization finished

--- scripts/regression/overall_linear_regression_statsmodels.py
+++ scripts/regression/overall_linear_regression_statsmodels.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import sklearn
-from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
 
 
 if __name__ == "__main__":
@@ -10,47 +9,31 @@ if __name__ == "__main__":
     df2 = pd.read_csv('data/raw_tract_data.csv')
     df_merged = pd.merge(df1, df2, on=['GEOID','inspection_year'], how='inner')
 
-    # #debug statements
-    # print(df_merged.columns)
-    # print(list(df_merged.iloc[0, :]))
-
     #linear regression:
     #regressors:
     #distance to floodzone, age, percent white, income, renter
     df_merged["white_percent"] = df_merged["total_white"] / df_merged["total_pop"]
-    print(df_merged["white_percent"])
     
     #set up the regressors and target
     final_df = df_merged[["distance_to_floodzone", "age", "white_percent", "income", "renter", "INSPECTION_SCORE"]]
    
     #data cleaning
     final_df = final_df.dropna()
-    print(final_df.head(10))
 
     X = final_df.iloc[: , 0:5]
-    print(X)
     y= final_df.iloc[:, 5]
-    print(y)
     
-    model =LinearRegression()
-    model.fit(X, y)
+    X_with_intercept = sm.add_constant(X)
 
-    #save 4 decimal digits for coefficients and intercepts
-    coef = list(map(lambda x: f'{x:.4e}', model.coef_))
-    intercept = round(model.intercept_, 4)
-    print(coef)
-    print(intercept)
+    # Fit the OLS model
+    model = sm.OLS(y, X_with_intercept)
+    results = model.fit()
 
     #format the equation
-    equa_file = open('figures/linear_regression/equation.txt','w')
+    equa_file = open('figures/linear_regression/summarytable.txt','w')
     equation = "INSPECTION_SCORE = "
-    i = 0
-    for feature in X.columns:
-        equation += f"{coef[i]}*{feature} + "
-        i += 1
-    equation += f"{str(intercept)}\n"
     equa_file.write("overall inspection score:\n")
-    equa_file.write(equation)
+    equa_file.write(str(results.summary()))
     equa_file.close()
 
     #visualization: correlation matrix
